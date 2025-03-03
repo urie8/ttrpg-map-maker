5b693a537fd52da2c1d8a9ad430285bd3b58ea54
Farre <farrezzz@gmail.com>
2025-03-03 20:38:32 +0100

    redone nosie page

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Simplex from "./Pages/Simplex";
 import BSP from "./Pages/BSP";
+import NoiseRenderTesting from "./Components/NoiseRenderTesting";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route
+          path="/NoiseRenderTesting"
+          element={<NoiseRenderTesting />}
+        />{" "}
         <Route path="/simplex" element={<Simplex />} />
         <Route path="/bsp" element={<BSP />} />
       </Routes>
-      <ColourfulCanvas />
     </Router>
   );
 }
